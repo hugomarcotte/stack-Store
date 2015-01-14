86948bb6c1a95b0332cbcec399216ac60069df85
Justin and Hugo <justincoh@gmail.com>
2015-01-14 11:51:55 -0500

    Added to seed file (Christian & Justin)

--- server/config/seed.js
+++ server/config/seed.js
@@ -34,6 +34,38 @@ Product.find({}).remove(function() {
     review: [],
     qty: 12,
     image: "http://underwrapsfitness.com.au/wp-content/uploads/2013/10/NEwProductComingSoon.jpg"
+  },{
+    name: 'Contraband',
+    price: 999999.99,
+    description: "You didn't buy this here",
+    category: [],
+    review: [],
+    qty: 9,
+    image: "http://milwaukee.brewers.mlb.com/mil/images/ballpark/y2012/im_noweapons_180x180.png"
+  },{
+    name: 'Headphones',
+    price: 999.99,
+    description: "The price is totally reasonable",
+    category: [],
+    review: [],
+    qty: 12,
+    image: "http://underwrapsfitness.com.au/wp-content/uploads/2013/10/NEwProductComingSoon.jpg"
+  },{
+    name: 'Dogs',
+    price: 1000,
+    description: 'How is this legal?',
+    category: [],
+    review: [],
+    qty: 12,
+    image: "http://www.prevention.com/sites/default/files/imagecache/slideshow_display/dog-dogue-de-bordeaux-puppy-410x290.jpg"
+  },{
+    name: 'Coffee',
+    price: 1.99,
+    description: 'Legal Drugs',
+    category: [],
+    review: [],
+    qty: 12,
+    image: "http://a.dryicons.com/images/icon_sets/travel_and_tourism_part_2/png/256x256/coffee.png"
   });
 });
 
@@ -50,6 +82,24 @@ User.find({}).remove(function() {
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
+  }, {
+    provider: 'local',
+    role: 'user',
+    name: 'frank',
+    email: 'frank@frank.com',
+    password: 'frank'
+  }, {
+    provider: 'local',
+    role: 'user',
+    name: 'bob',
+    email: 'bob@bob.com',
+    password: 'bob'
+  }, {
+    provider: 'local',
+    role: 'user',
+    name: 'jimbo',
+    email: 'jimbo@jimbo.com',
+    password: 'jimbo'
   }, function() {
       console.log('finished populating users');
     }
@@ -60,7 +110,7 @@ Category.find({}).remove(function(){
   Category.create(
     {name: 'Electronics'},
     {name: 'Furniture'},
-    {name: 'Jewelry'},
+    {name: 'Misc'},
     function(){
       console.log('Populated Categories')
     }
