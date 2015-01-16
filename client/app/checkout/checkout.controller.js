acca009024c33fc2b073e6a0d680ffadff8afb26
Pawel Socha <psocha@gmail.com>
2015-01-16 17:33:44 -0500

    Checkout process working for authenticated users

--- client/app/checkout/checkout.controller.js
+++ client/app/checkout/checkout.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('CheckoutCtrl', function ($scope, $cookieStore) {
+  .controller('CheckoutCtrl', function ($scope, $cookieStore, Order, Auth) {
     $scope.order = $cookieStore.get('cart');
     
     $scope.totalPriceCalculator = function(){
@@ -10,6 +10,29 @@ angular.module('stackStoreApp')
     		$scope.totalPrice += (item.product.price * item.qty);
     	})
     	return $scope.totalPrice;
+    }
+    $scope.placeOrder = function(order){
+    	var products = [];
+    	order.forEach(function(item){
+    		products.push({
+    		id: item.product._id,
+    		price: item.product.price,
+    		quantity: item.qty
+    		})
+    	})
+    	var user = Auth.getCurrentUser()._id;
+    	var date = new Date();
+    	var totalPrice = $scope.totalPrice;
+    	Order.save({_products:products,
+    		 _user:user, 
+    		 creationDate:date, 
+    		 totalPrice: totalPrice}
+    		 ,function(){ 
+    		 	//Should probably have something
+    		 	//diplaying success confirmation for user
+    		 	$cookieStore.remove('cart');
+    		 	
+    		 });
     }
 
   });
