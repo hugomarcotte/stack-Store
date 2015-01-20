a831ceb75f09678de92de4c82d80571d2ca503fe
Justin and Hugo <justincoh@gmail.com>
2015-01-20 11:48:16 -0500

    created new dashboard route

---
+++ client/app/dashboard/dashboard.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('stackStoreApp')
+  .controller('DashboardCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('stackStoreApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+      });
+  });
