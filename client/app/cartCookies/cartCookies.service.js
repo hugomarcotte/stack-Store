7e047c29580b79fab7828fbec049a567fa8acefa
justincoh <justincoh@gmail.com>
2015-01-20 09:26:06 -0500

    Removed console log

--- client/app/cartCookies/cartCookies.service.js
+++ client/app/cartCookies/cartCookies.service.js
@@ -30,10 +30,8 @@ angular.module('stackStoreApp')
         })
 
       } else {
-        var userId = Auth.getCurrentUser()._id || parseInt(Math.random()*1000000)+'-fake';
-        //Might also need to generate another cookie with the fake user Id
-        //depends on the rest of the controllers
-        console.log('USERID ',userId)
+        var userId = Auth.getCurrentUser()._id || parseInt(Math.random()*10000000)+'-fake';
+        //Not a great fake Id, but we'll never have 10m users
         var products=[{productId:productId,qty:numToAdd}];
         var newCart ={userId: userId, products: products}
           Cart.save(newCart,function(createdCart){
