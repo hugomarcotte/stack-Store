036fcf5d2f8e46ac3fffb082b84a20541f1e1894
Justin and Hugo <justincoh@gmail.com>
2015-01-20 11:42:04 -0500

    Pixel ping working, no page yet

---
+++ server/api/analytic/analytic.controller.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var _ = require('lodash');
+var Analytic = require('./analytic.model');
+
+// Get list of analytics
+exports.index = function(req, res) {
+  Analytic.find(function (err, analytics) {
+    if(err) { return handleError(res, err); }
+    return res.json(200, analytics);
+  });
+};
+
+// Get a single analytic
+exports.show = function(req, res) {
+  Analytic.findById(req.params.id, function (err, analytic) {
+    if(err) { return handleError(res, err); }
+    if(!analytic) { return res.send(404); }
+    return res.json(analytic);
+  });
+};
+
+// Creates a new analytic in the DB.
+exports.create = function(req, res) {
+  var ping = JSON.parse(req.body.json);
+  Analytic.create({page:ping}, function(err, analytic) {
+    if(err) { console.log(err); return handleError(res, err); }
+    return res.json(201, analytic);
+  });
+};
+
+// Updates an existing analytic in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Analytic.findById(req.params.id, function (err, analytic) {
+    if (err) { return handleError(res, err); }
+    if(!analytic) { return res.send(404); }
+    var updated = _.merge(analytic, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.json(200, analytic);
+    });
+  });
+};
+
+// Deletes a analytic from the DB.
+exports.destroy = function(req, res) {
+  Analytic.findById(req.params.id, function (err, analytic) {
+    if(err) { return handleError(res, err); }
+    if(!analytic) { return res.send(404); }
+    analytic.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.send(204);
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.send(500, err);
+}
---
+++ server/api/analytic/analytic.model.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var AnalyticSchema = new Schema({
+  page: {},
+  numHits: Number,
+  date: {type: Date,default:Date.now()}
+});
+
+module.exports = mongoose.model('Analytic', AnalyticSchema);
